b0dcb4292f90a08e4209fdd85445e30a8aeddbef
LettieriFrancesco <134380483+LettieriFrancesco@users.noreply.github.com>
2025-05-23 22:35:38 +0200

    Vari fix css

---
+++ HaveASeat/Controllers/PartnerController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HaveASeat.Controllers
+{
+    //[Authorize(Roles = "Partner")]
+    public class PartnerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
