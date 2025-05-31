fe759fe8c83fcd202b1dce69bd7b74acb506f070
Paolo Leucio Manca <132942470+Paolomanca90@users.noreply.github.com>
2025-05-31 16:52:57 +0100

    nuova sede

--- HaveASeat/Controllers/PartnerController.cs
+++ HaveASeat/Controllers/PartnerController.cs
@@ -292,6 +292,52 @@ namespace HaveASeat.Controllers
 			return View(salone);
 		}
 
+		public IActionResult CreateSede()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult CreateSede(Salone model)
+		{
+			if (!ModelState.IsValid)
+				return View(model);
+
+			var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+			if (userId == null)
+			{
+				return RedirectToAction("Error", "Home");
+			}
+			var user = _context.Users.Find(userId);
+			if (user == null)
+			{
+				return RedirectToAction("Error", "Home");
+			}
+
+			var salone = new Salone
+			{
+				Nome = model.Nome,
+				Indirizzo = model.Indirizzo,
+				Citta = model.Citta,
+				Provincia = model.Provincia,
+				CAP = model.CAP,
+				Regione = model.Regione,
+				Telefono = model.Telefono,
+				Email = model.Email,
+				SitoWeb = model.SitoWeb,
+				PartitaIVA = model.PartitaIVA,
+				RagioneSociale = model.RagioneSociale,
+				SDI = model.SDI,
+				ApplicationUserId = userId,
+				ApplicationUser = user,
+			};
+
+			_context.Salone.Add(salone);
+			_context.SaveChanges();
+
+			return RedirectToAction("CreateCheckoutSession", "Partner", new { id = userId });
+		}
+
 		[HttpGet]
 		public IActionResult EditSede(Guid id)
 		{
