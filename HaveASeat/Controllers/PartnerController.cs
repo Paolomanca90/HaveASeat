4b5195c39a7a6eda507167436d7617efe7f6b1a7
LettieriFrancesco <134380483+LettieriFrancesco@users.noreply.github.com>
2025-07-01 01:49:49 +0200

    fix

--- HaveASeat/Controllers/PartnerController.cs
+++ HaveASeat/Controllers/PartnerController.cs
@@ -66,7 +66,10 @@ namespace HaveASeat.Controllers
 			}
 
 			viewModel.SaloneCorrente = viewModel.SaloniUtente.First(s => s.SaloneId == viewModel.SelectedSaloneId);
-			var abbonamentoStandard = viewModel.SaloneCorrente.SaloneAbbonamenti.Any(x => x.AbbonamentoId == SubscriptionsConstants.Basic);
+            ViewBag.HasMultipleSedi = viewModel.SaloniUtente.Count > 1;
+			ViewBag.Saloni = viewModel.SaloniUtente;
+			viewModel.se
+            var abbonamentoStandard = viewModel.SaloneCorrente.SaloneAbbonamenti.Any(x => x.AbbonamentoId == SubscriptionsConstants.Basic);
 			if (abbonamentoStandard)
 				ViewBag.Basic = true;
 			// Calcola le date del periodo
